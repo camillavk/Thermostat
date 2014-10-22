a0b3ec1f2400a16f3f95a332d6bd251793c57ab6
Camilla van Klinken <camillavk@gmail.com>
2014-10-22 11:35:50 +0100

    min and max temperatures set

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -1,12 +1,66 @@
+"use strict";
 describe('thermostat', function() {
 
 	var thermostat
 
+	beforeEach(function() {
+		thermostat = new Thermostat
+	});
+
 	describe('by default', function() {
 
 		it('it is set to 20 degrees', function() {
-			thermostat = new Thermostat
 			expect(thermostat.temperature).toEqual(20)
+		});
+
+		it('power saving mode will be on', function() {
+			expect(thermostat.isPowerSaverOn).toBe(true)
+		});
+
+		it('it can increase the temperature by 1 degree', function() {
+			thermostat.increaseTemperature()
+			expect(thermostat.temperature).toEqual(21)
+		});
+
+		it('it can decrease the temperature by 1 degree', function() {
+			thermostat.decreaseTemperature()
+			expect(thermostat.temperature).toEqual(19)
+		});
+
+		it('has a minimum temperature of 10 degrees', function() {
+			expect(thermostat.minTemperature).toEqual(10)
+		});
+
+		xit('cannot go below the minimum temperature', function() {
+			expect(thermostat.decreaseTemperatureBy(11)).toThrow(new Error("That's too cold!"))
+		});
+
+		it('if power saving mode is on the max temp is 25', function() {
+			expect(thermostat.maxTemperature).toEqual(25)
+		});
+
+	});
+
+	describe('custom features', function() {
+
+		it('it can increase the temperature by 5 degrees', function() {
+			thermostat.increaseTemperatureBy(5)
+			expect(thermostat.temperature).toEqual(25)
+		});
+
+		it('it can decrease the temperature by 5 degrees', function() {
+			thermostat.decreaseTemperatureBy(5)
+			expect(thermostat.temperature).toEqual(15)
+		});
+
+		it('it can turn of the power saving mode', function() {
+			thermostat.turnPowerSaverOff()
+			expect(thermostat.isPowerSaverOn).toBe(false)
+		});
+
+		it('if power saving mode is off, the max temp is 32', function() {
+			thermostat.turnPowerSaverOff()
+			expect(thermostat.maxTemperature).toEqual(32)
 		});
 
 	});
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,3 +1,40 @@
 function Thermostat() {
 	this.temperature = 20;
+	this.isPowerSaverOn = true;
+	this.minTemperature = 10;
+	this.maxTemperature = 25;
 }
+
+Thermostat.prototype.increaseTemperature = function() {
+	this.increaseTemperatureBy(1);
+};
+
+Thermostat.prototype.increaseTemperatureBy = function(degrees) {
+	this.temperature += degrees;
+};
+
+Thermostat.prototype.decreaseTemperature = function() {
+	this.decreaseTemperatureBy(1);
+};
+
+Thermostat.prototype.decreaseTemperatureBy = function(degrees) {
+	this.temperature -= degrees;
+	if (this.temperature < this.minTemperature) {
+		throw new Error("That's too cold!")
+	}
+	return this.temperature
+};
+
+Thermostat.prototype.turnPowerSaverOff = function() {
+	this.isPowerSaverOn = false;
+	this.maxTemperature = 32;
+};
+
+
+// Thermostat.prototype.maxTemperature = function() {
+// 	if (this.isPowerSaverOn = false) {
+// 		this.maxTemperature += 7
+// 	}
+	// else if (this.isPowerSaverOn = false) {
+	// 	this.maxTemperature = 32
+	// }
